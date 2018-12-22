640b7ad556ce4be7e141ffc2ffa102a6b7ad5c48
Ronan Le Hy <ronan.lehy@gmail.com>
2018-12-22 23:08:08 +0100

    Adjust settings for paperspace.

--- openimages/settings.py
+++ openimages/settings.py
@@ -1,5 +1,10 @@
 import os
 
-data_directory = '/mnt/data/openimages'
-openimages_directory = os.path.join(data_directory, "openimages-v4/bb")
-log_directory = '/mnt/data'
+persistent_directory = '/storage'
+published_directory = '/artifacts'
+fast_directory = '/workspace'
+
+cold_data_directory = os.path.join(persistent_directory, 'data')
+hot_data_directory = os.path.join(fast_directory, 'data')
+openimages_directory = os.path.join(hot_data_directory, "openimages-v4/bb")
+log_directory = os.path.join(persistent_directory, 'logs')
