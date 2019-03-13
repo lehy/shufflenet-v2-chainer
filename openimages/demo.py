04572515dc4a95dd1a16acc3e1b1d967b639edc4
Ronan Le Hy <ronan.lehy@gmail.com>
2019-03-13 13:40:09 +0100

    demo: add support for gpu/cpu on command line

--- openimages/demo.py
+++ openimages/demo.py
@@ -6,7 +6,7 @@ import numpy as np
 import time
 
 
-def main(argv):
+def main(args):
     with chainer.using_config('train', False):
         with chainer.using_config('enable_backprop', False):
             snapshot_file = "shufflenet-v2-snapshots/x1/snapshot_iter_335305"
@@ -15,7 +15,8 @@ def main(argv):
             net = shufflenet_v2.ShuffleNetV2(1, label_encoder.num_classes())
             chainer.serializers.load_npz(
                 snapshot_file, net, "updater/model:main/predictor/")
-            # net.to_gpu()
+            if args.gpu >= 0:
+                net.to_gpu(args.gpu)
 
             camera_id = -1
             camera = cv2.VideoCapture(camera_id)
@@ -36,7 +37,7 @@ def main(argv):
                 output = chainer.functions.sigmoid(output)
                 output = chainer.cuda.to_cpu(output.data[0])
                 t1 = time.time()
-                
+
                 labels_idx = np.where(output > 0.5)[0]
                 readable_labels = [label_encoder.readable_label_of_encoded_label(
                     lab) for lab in labels_idx]
@@ -50,6 +51,14 @@ def main(argv):
                 cv2.waitKey(1)
 
 
+def parse_command_line():
+    # TODO: add command line args for snapshot
+    parser = argparse.ArgumentParser(
+        description="Demonstration of multilabel classification with Shufflenet v2.")
+    parser.add_argument('--gpu', help='Run on gpu (integer id starting at 0) or cpu (-1)', type=int, default=-1)
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    import sys
-    main(sys.argv)
+    import argparse
+    args = parse_command_line()
+    main(args)
